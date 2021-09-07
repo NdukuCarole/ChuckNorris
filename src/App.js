1991cf34fb40a6e0b1b2225d71385efcbbcf0d66
kahlinah <ndukucarole@gmail.com>
2021-09-07 11:58:11 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {CssBaseline, Typography} from "@material-ui/core";
+
+function App() {
+  return (
+    <div className="App">
+      <CssBaseline/>
+      <Typography variant="h1" align="center">
+        Chuck Norris Jokes </Typography>
+      
+    </div>
+  );
+}
+
+export default App;
