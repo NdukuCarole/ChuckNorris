3abbd9b372076bfebc9fc28b575183d5b7f214e4
kahlinah <54279456+kahlinah@users.noreply.github.com>
2021-09-09 08:02:22 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import {CssBaseline, Typography} from "@material-ui/core";
+import {CssBaseline, Container, Typography} from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
+    <Container>
       <CssBaseline/>
       <Typography variant="h1" align="center">
         Chuck Norris Jokes </Typography>
+    </Container>
       
     </div>
   );
